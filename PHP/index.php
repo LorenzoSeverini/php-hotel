4619d37f6c0ab775ea9774f438f51a68ecc7eefd
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-08 16:48:26 +0200

    add table with bootstrap and link file style css

--- PHP/index.php
+++ PHP/index.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <!-- CSS -->
+    <link rel="stylesheet" href="../css/style.css">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
@@ -76,24 +77,36 @@
 
     <!-- main -->
     <main>
-        <!-- hotels array printed with informations using foreach -->
-        <?php
-        foreach ($hotels as $hotel) { ?>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div>
-                            <h2><?php echo $hotel['name']; ?></h2>
-                            <p>Description: <?php echo $hotel['description']; ?></p>
-                            <p>Parking: <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></p>
-                            <p>Vote: <?php echo $hotel['vote']; ?></p>
-                            <p>Distance to center: <?php echo $hotel['distance_to_center']; ?> km</p>
-                        </div>
-                    </div>
+        <!-- container -->
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- table -->
+                    <table class="table table-striped table-hover border border-danger m-3">
+                        <thead>
+                            <tr class="table-primary text-center">
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Parking</th>
+                                <th>Vote from 0 to 5</th>
+                                <th>Distance to center</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-dark">
+                            <?php foreach ($hotels as $hotel) { ?>
+                                <tr class="text-center">
+                                    <td><?php echo $hotel['name']; ?></td>
+                                    <td><?php echo $hotel['description']; ?></td>
+                                    <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
+                                    <td><?php echo $hotel['vote']; ?></td>
+                                    <td><?php echo $hotel['distance_to_center']; ?>km</td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        <?php }
-        ?>
+        </div>
     </main>
 
     <!-- footer -->
