0a4a66896c987d065552e922e0f7f7f609c7f883
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-08 16:11:41 +0200

    printed the hotels array information on the page

--- PHP/index.php
+++ PHP/index.php
@@ -6,8 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" href="../CSS/style.css">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
@@ -17,6 +20,7 @@
 
     <?php
 
+    // hotels array
     $hotels = [
 
         [
@@ -59,10 +63,53 @@
 
     ?>
 
+    <!-- header -->
+    <header class="bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-white text-center">Hotels</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- main -->
+    <main>
+        <!-- hotels array printed with informations using foreach -->
+        <?php
+        foreach ($hotels as $hotel) { ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div>
+                            <h2><?php echo $hotel['name']; ?></h2>
+                            <p>Description: <?php echo $hotel['description']; ?></p>
+                            <p>Parking: <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></p>
+                            <p>Vote: <?php echo $hotel['vote']; ?></p>
+                            <p>Distance to center: <?php echo $hotel['distance_to_center']; ?> km</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php }
+        ?>
+    </main>
+
+    <!-- footer -->
+    <footer class="bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-white text-center p-2 m-0">Footer</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <!-- script -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
